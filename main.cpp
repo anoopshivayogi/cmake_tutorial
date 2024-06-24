d3851a10ea72d193ffb06d5832f071e6389d0eea
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-06-23 21:22:51 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+ 
+//  The above code is a simple C++ program that prints “Hello, World!” to the console. 
+//  Step 2: Create a CMakeLists.txt file 
+//  Next, create a CMakeLists.txt file in the root directory of your project. This file is used by CMake to configure the project. 
+//  # CMakeLists.txt
